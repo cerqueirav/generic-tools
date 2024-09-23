3bb69c64f80941bbd1dc3593c6b7c525a5161a51
Victor Cerqueira <victorcerx@gmail.com>
2024-09-23 06:59:29 -0300

    -- Adiciona a documentação novamente a controller

--- generic-tools-api/generic-tools-api/Controllers/LocalizacaoController.cs
+++ generic-tools-api/generic-tools-api/Controllers/LocalizacaoController.cs
@@ -15,6 +15,11 @@ namespace GenericToolsAPI.Controllers
             _localizacaoService = localizacaoService;
         }
 
+        /// <summary>
+        /// Obtém a geolocalização a partir do endereço fornecido (Rua, Número, Bairro, Cidade, País).
+        /// </summary>
+        /// <param name="endereco">Objeto contendo os dados do endereço.</param>
+        /// <returns>Os dados de geolocalização em formato JSON.</returns>
         [HttpPost("geolocalizacao")]
         public async Task<IActionResult> Geolocalizacao([FromBody] EnderecoRequest endereco)
         {
@@ -22,6 +27,11 @@ namespace GenericToolsAPI.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Realiza a geolocalização reversa a partir das coordenadas fornecidas (Latitude, Longitude).
+        /// </summary>
+        /// <param name="coords">Objeto contendo as coordenadas (Latitude e Longitude).</param>
+        /// <returns>Os dados de geolocalização em formato JSON.</returns>
         [HttpPost("geolocalizacao-reversa")]
         public async Task<IActionResult> GeolocalizacaoReversa([FromBody] CoordenadasRequest coords)
         {
@@ -29,6 +39,11 @@ namespace GenericToolsAPI.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Obtém os limites geográficos da cidade com base no nome da cidade, estado e país fornecidos.
+        /// </summary>
+        /// <param name="request">Objeto contendo os dados da cidade, estado e país.</param>
+        /// <returns>Os limites geográficos da cidade em formato JSON.</returns>
         [HttpPost("limites-cidade")]
         public async Task<IActionResult> LimitesCidade([FromBody] LimiteCidadeRequest request)
         {
@@ -36,6 +51,11 @@ namespace GenericToolsAPI.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Busca Pontos de Interesse (POI) com base no tipo, cidade, estado e país fornecidos.
+        /// </summary>
+        /// <param name="poiRequest">Objeto contendo os dados do tipo de POI, cidade, estado e país.</param>
+        /// <returns>A lista de Pontos de Interesse em formato JSON.</returns>
         [HttpPost("buscar-poi")]
         public async Task<IActionResult> BuscarPOI([FromBody] PontoInteresseRequest poiRequest)
         {
@@ -43,6 +63,10 @@ namespace GenericToolsAPI.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Obtém o IP público do usuário.
+        /// </summary>
+        /// <returns>O IP público em formato JSON.</returns>
         [HttpGet("ip-publico")]
         public async Task<IActionResult> ObterIpPublico()
         {
